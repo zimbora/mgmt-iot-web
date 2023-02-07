7d036d7296d3a1ae63604d08fd68ba00ff1a779f
João Silva <jsilva@thinkdigital.pt>
2023-02-07 17:37:38 +0000

    removes port from url and api link

--- server/public/js/settings.js
+++ server/public/js/settings.js
@@ -1,8 +1,8 @@
 var Settings = {
-  url : window.location.protocol+"//"+window.location.hostname+":"+window.location.port, //"http://10.168.1.69:4000",
-  //url : window.location.protocol+"//"+window.location.hostname, //"http://10.168.1.69:4000",
-  api : window.location.protocol+"//"+window.location.hostname+":"+window.location.port+"/api", //"http://10.168.1.69:4000/api"
-  //api : window.location.protocol+"//"+window.location.hostname+"/api", //"http://10.168.1.69:4000/api"
+  //url : window.location.protocol+"//"+window.location.hostname+":"+window.location.port, //"http://10.168.1.69:4000",
+  url : window.location.protocol+"//"+window.location.hostname, //"http://10.168.1.69:4000",
+  //api : window.location.protocol+"//"+window.location.hostname+":"+window.location.port+"/api", //"http://10.168.1.69:4000/api"
+  api : window.location.protocol+"//"+window.location.hostname+"/api", //"http://10.168.1.69:4000/api"
   mqtt : {
     host : location.hostname,
     port : 8888,
