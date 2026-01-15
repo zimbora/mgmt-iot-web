272c966cc4211967c836966095a8604dadfab827
zimbora <lucas.ua.eet@gmail.com>
2026-01-15 20:08:33 +0000

    fix mqtt web connection Get parameters from config file or use default configurations

--- server/public/js/settings.js
+++ server/public/js/settings.js
@@ -1,9 +1,10 @@
+// file not being used!!
 var Settings = {
   url : window.location.protocol+"//"+window.location.hostname+":"+window.location.port,
   api : window.location.protocol+"//"+window.location.hostname+":"+window.location.port+"/api",
   mqtt : {
     host : window.location.hostname,
-    port : 8888,
-    ssl : false
+    port : 443,
+    ssl : true
   }
 };
